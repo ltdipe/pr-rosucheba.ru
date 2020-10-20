82d1569a20b62e6f1257a70d2823dda8aef77344
Novailoveyou <novailoveyoux@gmail.com>
2020-10-20 18:46:06 +0300

    improved popup forms ui

--- development/main.fb6bbcaf.js
+++ development/main.fb6bbcaf.js
@@ -3670,7 +3670,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53789" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51073" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
